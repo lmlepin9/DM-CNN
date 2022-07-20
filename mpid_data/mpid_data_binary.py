d4917ab15eab4d8e796a6bb4e83bf620b69f8e87
Yuliia Maidannyk <juliia.maydannik@gmail.com>
2022-07-20 13:42:22 +0100

    h5py usage

--- mpid_data/mpid_data_binary.py
+++ mpid_data/mpid_data_binary.py
@@ -1,9 +1,7 @@
 import numpy as np
-from larcv import larcv
 import random
 
-import ROOT
-from ROOT import TChain
+import h5py
 
 import torch
 from torch.utils.data import Dataset
@@ -13,6 +11,13 @@ def image_modify(img):
     img_mod = np.where(img>500, 500,img_mod)
     return img_mod
 
+def EvDisp(input_file):
+    producer = 'image2d_binary'
+    f = h5py.File(input_file,'r')
+    event_id_list = f['eventid']
+    wire_set = f['image2d'][producer]
+    image=wire_set[list(wire_set.keys())[0]]
+    return event_id_list, image
 
 #Plane 2 is the only one present in the cropped dataset, therefore we use plane = 0 
 class MPID_Dataset(Dataset):
@@ -21,9 +26,10 @@ class MPID_Dataset(Dataset):
         self.augment=augment
         self.verbose=verbose
          
+        self.input_file = input_file
+        #f = h5py.File(input_file,'r')
+        #self.particle_image_chain = f['image2d']['image2d_binary_tree']
 
-        self.particle_image_chain = TChain(image_tree)
-        self.particle_image_chain.AddFile(input_file)
         if (device):
             self.device=device
         else:
@@ -32,42 +38,34 @@ class MPID_Dataset(Dataset):
     def __getitem__(self, ENTRY):
         # Reading Image
 
-        #print ("open ENTRY @ {}".format(ENTRY))
-
-        self.particle_image_chain.GetEntry(ENTRY)
-        self.this_image_cpp_object = self.particle_image_chain.image2d_image2d_binary_branch
+        #self.particle_image_chain.GetEntry(ENTRY)
+        #self.this_image_cpp_object = self.particle_image_chain.image2d_image2d_binary_branch
         
-        self.event_info = torch.zeros([3])
-        self.event_info[0] = self.this_image_cpp_object.run()
-        self.event_info[1] = self.this_image_cpp_object.subrun()
-        self.event_info[2] = self.this_image_cpp_object.event()
-        self.this_image=larcv.as_ndarray(self.this_image_cpp_object.as_vector()[self.plane])
-        # Image Thresholding
-        self.this_image=image_modify(self.this_image)
+        self.event_info = [[],[],[]]
 
-        #print (self.this_image)
-        #print ("sum, ")
-        #if (np.sum(self.this_image) < 9000):
-        #    ENTRY+
+        self.event_id_list, self.this_image = EvDisp(self.input_file)
+
+        for element in self.event_id_list:
+            self.event_info[0] = element[1] # run
+            self.event_info[1] = element[2] # subrun
+            self.event_info[2] = element[3] # event
+
+        # Image Thresholding
+        #self.this_image=image_modify(self.this_image)
         
         if self.augment:
             if random.randint(0, 1):
-            #if True:
-                #if (self.verbose): print ("flipped")
                 self.this_image = np.fliplr(self.this_image)
             if random.randint(0, 1):
-            #if True:
-                #if (self.verbose): print ("transposed")
                 self.this_image = self.this_image.transpose(1,0)
-        self.this_image = torch.from_numpy(self.this_image.copy())
-        # self.this_image=torch.tensor(self.this_image, device=self.device).float()
+        #self.this_image = torch.from_numpy(self.this_image.copy())
 
-        self.this_image=self.this_image.clone().detach()
+        #self.this_image=self.this_image.clone().detach()
         
         # Creating labels 
         self.event_label = torch.zeros([2])
         # Signal events are labeled with run = 100 
-        if(self.this_image_cpp_object.run() == 100):
+        if(self.event_info[0] == 100):
             self.event_label[0] = 1
         
         else:
@@ -77,6 +75,8 @@ class MPID_Dataset(Dataset):
         return (self.this_image, self.event_label, self.event_info)
         #return (self.this_image, self.event_label)
 
-    def __len__(self):
-        return self.particle_image_chain.GetEntries()
+    #def __len__(self):
+        #return self.particle_image_chain.GetEntries()
 
+train_file = "/Users/juliamaidannyk/Downloads/dark_trident_train_overlay_larcv_cropped.h5"
+train_data = MPID_Dataset(train_file, "image2d_image2d_binary_tree", device='cpu', plane=0, augment=False)
